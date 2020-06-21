bf94d408780137f663f25529e48e75bb7101788d
Nicolas <ndonosofs@gmail.com>
2020-06-20 23:15:26 -0400

    Timer done

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,13 +1,48 @@
 <script>
  import ProgressBar from './ProgressBar.svelte';
+
+const totalSeconds = 20;
+
+let secondLeft = totalSeconds;
+let isRunning = false;
+let buttonText = "Iniciar";
+
+$: progress = (totalSeconds - secondLeft) / totalSeconds * 100;
+
+function startTimer() {
+     isRunning = true;
+     buttonText = "Lavando manos...";
+const timer = setInterval(() => {
+    secondLeft -= 1;
+    if(secondLeft==0){
+        clearInterval(timer);
+        isRunning = false;
+        buttonText = "Re Iniciar";
+        secondLeft = totalSeconds;
+    }
+}, 1000);
+
+}
+
+
+    
+    
+
+
+
 </script>
 
-<ProgressBar />
+
 <div bp="grid">
-<div bp="offset-5@md 4@md 12@sm">
-<h2>Seconds Left:</h2>
-<button class="start">Start</button>
+
+<h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondLeft}</h2>
+
 </div>
+
+<ProgressBar {progress} />
+
+<div bp="grid">
+<button disabled={isRunning} on:click={startTimer} bp="offset-5@md 4@md 12@sm" class="start">{buttonText}</button>
 </div>
 
 <style>
@@ -15,6 +50,9 @@
 background-color:rgb(154,73,73);
 width:100%;
 margin:10px 0px;
-
+ }
+ .start[disabled]{
+background-color: rgb(194,194,194);
+cursor: not-allowed;
  }
 </style>
